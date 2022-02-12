32c7192b28923c8408f348acf03c39dac1962bb2
koko199603 <99552563+koko199603@users.noreply.github.com>
2022-02-13 00:46:24 +0700

    Update wagir.php

--- wagir.php
+++ wagir.php
@@ -1,29 +1,3 @@
-<?php
-error_reporting(E_ALL ^ E_WARNING);
-error_reporting(E_ALL ^ E_DEPRECATED);
-welcome:
-function curl($url,$headers,$data){
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    $resp = curl_exec($curl);
-    curl_close($curl);
-    return $resp;
-    }
-$random1 = rand(11111111,99999999);
-$random2 = rand(11111111,99999999);
-$random3 = rand(1119,9999);
-$random4 = rand(1,9999);
-
-$deviceid= "$random1-936d-974b-a9ef-$random2$random3";
-$deviceid2 = "\"$deviceid\"";
-echo "\n";
-echo "\n";
 os.system('clear');
 include "fcgomart.php";
 ulang:
